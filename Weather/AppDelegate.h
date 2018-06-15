c6dabdeffca5abe7a03a411a73af7a94d00acfb8
jian <jiantutk@gmail.com>
2018-06-15 13:26:16 -0400

    Initial Commit

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by Jian on 6/15/18.
+//  Copyright © 2018 Jian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by Jian on 6/15/18.
+//  Copyright © 2018 Jian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
